796a8411c1e2e098b863472a40e7c6242d6108bf
Valera Satsura <satsura@gmail.com>
2019-03-21 11:09:11 +0300

    fix caching image

--- src/ClockV2.php
+++ src/ClockV2.php
@@ -158,7 +158,7 @@ class ClockV2 {
         //expire this image instantly
         header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-        header('Cache-Control: no-store, no-cache, must-revalidate');
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
         header('Cache-Control: post-check=0, pre-check=0', false);
         header('Pragma: no-cache');
         //generate a new GIF given the frames, the delay and the loop counter
